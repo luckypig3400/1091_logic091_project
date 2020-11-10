eada707530e717af1ab210a9435f5bb87cb423b9
郵差 <a34007024@gmail.com>
2020-11-10 17:51:32 +0800

    Create 20201110practice3_3peopleScore_and_sort.c
    copy from 20201110practice2_3peopleScore

---
+++ 20201110practice3_3peopleScore_and_sort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    int score[3][5];
+    /*
+    ch en math sum average
+    [] [] []   []  []  
+    [] [] []   []  []
+    [] [] []   []  []
+    */
+   int searchID = 0;
+
+   for(int i=0;i<3;i++){
+       for(int j=0;j<3;j++){
+           if(j == 0)printf("請輸入第%d位同學的國文成績：",i+1);
+           else if(j==1)printf("請輸入第%d位同學的英文成績：",i+1);
+           else printf("請輸入第%d位同學的數學成績：",i+1);
+
+           scanf("%d",&score[i][j]);
+       }
+   }
+
+    printf("===========================\n");
+    printf("請輸入想查詢成績的同學編號:");
+    scanf("%d",&searchID);
+    searchID -= 1;//讓查詢的位置符合實際存在陣列上位置
+    printf("===========================\n");
+
+    printf("第%d位同學的成績為：國文%d分，英文%d分，數學%d分",searchID+1,score[searchID][0],score[searchID][1],score[searchID][2]);
+
+    return 0;
+}
