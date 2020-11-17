897013dd91860df3cb314982dd0f61ed7d7e5630
郵差 <a34007024@gmail.com>
2020-11-17 17:38:04 +0800

    fix can't input name problem in second input

--- 20201117practice1_inputNameAndTEL.c
+++ 20201117practice1_inputNameAndTEL.c
@@ -7,6 +7,7 @@ int main()
     char name[5][64];
     char phone[5][64];
     char inputCache[64];
+    char continueInput[3];
 
     // 假設宣告name[3][6]則:
     // 0 1 2 3 4 5
@@ -26,19 +27,25 @@ int main()
     { //不可超過字元陣列範圍
         printf("請輸入姓名：");
         gets(inputCache);
-        printf("%s\n",inputCache);//debug
-        strcpy(name[inputCount], inputCache);//複製string
+        strcpy(name[inputCount], inputCache); //複製string
 
         printf("請輸入電話：");
         gets(inputCache);
-        printf("%s\n",inputCache);//debug
-        strcpy(phone[inputCount], inputCache);//複製string
+        strcpy(phone[inputCount], inputCache); //複製string
 
         printf("%s\t", name[inputCount]);
         printf("%s\n", phone[inputCount]);
 
+        printf("是否繼續輸入(y/n):");
+        gets(continueInput);
+        if(continueInput[0] == 'n' || continueInput[0] == 'N') break;
+        // else do nothing(默認繼續輸入)
+        printf("===========================\n");
+
         inputCount++;
     }
+
+    
 
     return 0;
 }
