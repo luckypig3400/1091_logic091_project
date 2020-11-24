ebcdebd6c7c98628af70d5bdda144f0b0fe5eca4
郵差 <a34007024@gmail.com>
2020-11-24 15:56:59 +0800

    fix password compare ignore letter case

--- 20201124practice1.c
+++ 20201124practice1.c
@@ -6,7 +6,7 @@ int main()
 
     char user[10][16] = {"User1", "User2", "User3", "User4", "User5",
                          "User6", "User7", "User8", "User9", "User10"};
-    char pwd[10][16] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+    char pwd[10][16] = {"pwd1", "2", "pwd3", "4", "5", "pwd6", "7", "8", "pwd9", "10"};
 
     char userInput[16];
     char pwdInput[16];
@@ -19,9 +19,9 @@ int main()
 
     for (int i = 0; i < 10; i++)
     {
-        if (stricmp(user[i], userInput) == 0)
+        if (stricmp(user[i], userInput) == 0)//stricmp 不計大小寫之字串比較
         { //找到帳號
-            if (stricmp(pwd[i], pwdInput) == 0)
+            if (strcmp(pwd[i], pwdInput) == 0)//strcmp 計大小寫之字串比較
             {
                 printf("帳號密碼正確");
             }
