e546396e967ab448c5d534f4fbc79bee7bb5262a
郵差 <a34007024@gmail.com>
2020-11-24 17:13:44 +0800

    use getche to store input and convert to int

--- midterm_calculator_try.c
+++ midterm_calculator_try.c
@@ -1,15 +1,44 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
 
-    char str[16];
-    printf("Please input string:");
-    gets(str); //抓一字串。(可包含空格)
-    printf("Input String is:%s", str);
-    for (int i = 0; i < 16; i++)
+    char inputArray[16];
+    char input;
+    int inputCount = 0;
+    int intArray[16];
+    int arraySize = 16;
+    printf("請輸入一四則運算式:");
+
+    for (int i = 0; i < arraySize; i++)
+        inputArray[i] = '\0'; //inital char Array
+
+    do
     {
-        printf("str[%d]=%d   ", i, str[i]);
+        input = getche();
+        inputArray[inputCount] = input;
+        inputCount += 1;
+    } while (input != '=' && inputCount != 99);
+
+    for (int i = 0; i < arraySize; i++)
+    {
+        if (inputArray[i] != '=' || inputArray[i] != '+' || inputArray[i] != '-' || inputArray[i] != '*' || inputArray[i] != '/')
+            intArray[i] = inputArray[i] - '0';
+        //https://stackoverflow.com/questions/5029840/convert-char-to-int-in-c-and-c
+    }
+
+    printf("\n輸入的運算式為:\n");
+    for (int i = 0; i < arraySize; i++)
+    {
+        if (inputArray[i] != '=')
+            printf("str[%d]=%c\t", i, inputArray[i]);
+    }
+
+    printf("\n輸入的數字共有以下幾個:\n");
+    for (int i = 0; i < arraySize; i++)
+    {
+        printf("int[%d]=%d\t", i, intArray[i]);
     }
 
     return 0;
