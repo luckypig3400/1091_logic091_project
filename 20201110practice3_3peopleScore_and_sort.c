a3aa40b7497147a7fb612b148615b1b1fd31f592
郵差 <a34007024@gmail.com>
2020-11-24 12:03:43 +0800

    sort sum of score and original id

--- 20201110practice3_3peopleScore_and_sort.c
+++ 20201110practice3_3peopleScore_and_sort.c
@@ -19,7 +19,7 @@ int main()
         for (int j = 0; j < 6; j++)
         {
             if (j == 5)
-                score[i][j] = i;
+                score[i][j] = i; //write original ID
             else
                 score[i][j] = 0;
         }
@@ -42,8 +42,35 @@ int main()
         score[i][4] = score[i][3] / 3;                         //計算平均
     }
 
-    for (searchID = 0; searchID < 3; searchID++){//輸出全部結果(未排序)
+    printf("======未按照成績排序======\n");
+    for (searchID = 0; searchID < 3; searchID++)
+    { //輸出全部結果(未排序)
         printf("第%d位同學的成績為：國文%.0f分，英文%.0f分，數學%.0f分\t總分:%.0f分\t平均:%.2f分 \n", searchID + 1, score[searchID][0], score[searchID][1], score[searchID][2], score[searchID][3], score[searchID][4]);
+    }
+
+    for (int i = 0; i < 3; i++) //排序區塊(小到大)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (score[i][3] < score[j][3]) //[][3]第三格為總分
+            {
+                int swap = score[i][3];
+                score[i][3] = score[j][3];
+                score[j][3] = swap;
+
+                int orgID = score[i][5]; //總分排序完後搬移原始ID儲存格
+                score[i][5] = score[j][5];
+                score[j][5] = orgID;
+            }
+        }
+    }
+
+    //TODO 按照原始ID搬移剩餘的成績
+
+    printf("======已按照成績排序======\n");
+    for (int searchID = 0; searchID < 3; searchID++)
+    {
+        printf("(原始ID:%d)第%d位同學的成績為：國文%.0f分，英文%.0f分，數學%.0f分\t總分:%.0f分\t平均:%.2f分 \n", score[searchID][5], searchID + 1, score[searchID][0], score[searchID][1], score[searchID][2], score[searchID][3], score[searchID][4]);
     }
 
     return 0;
