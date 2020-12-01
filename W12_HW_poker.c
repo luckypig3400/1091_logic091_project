ec0560b6285a31c9e7fd48f8030a1e1fe449a61a
郵差 <a34007024@gmail.com>
2020-12-01 16:50:27 +0800

    add random poker and classify output to 4 groups

--- W12_HW_poker.c
+++ W12_HW_poker.c
@@ -1,21 +1,31 @@
 #include <stdio.h>
-#include <string.h>
 
 int main()
 {
 
     int originalPoker[52];
-    for (int i = 0; i < 52; i++)
+    int randomPoker[52];
+    for (int i = 0; i < 52; i++) //產生原始順序的撲克牌
     {
         originalPoker[i] = i + 1;
         //1 ~ 52
     }
 
-    for (int i = 0; i < 52; i++)
+    for (int i = 0; i < 52; i++) //產生打亂順序的撲克牌
     {
+        int j = rand() % 52;
+        int swap = randomPoker[i];
+        randomPoker[i] = randomPoker[j];
+        randomPoker[j] = swap;
+    }
+
+    for (int i = 0; i < 52; i++) //輸出原始撲克牌
+    {
+        if (i % 13 == 0 && i != 0)
+            printf("\n"); //每13張分一組
         if (originalPoker[i] <= 13)
         {
-            printf("♠%d", originalPoker[i]);
+            printf("♠%d ", originalPoker[i]);
         }
         else if (originalPoker[i] <= 26)
         {
