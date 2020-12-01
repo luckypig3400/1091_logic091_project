0154b67f1a96094a577869869df28c1d2a827991
郵差 <a34007024@gmail.com>
2020-12-01 18:43:04 +0800

    add comment on include library

--- 20201201_W13_HW_sortW12randomPoker.c
+++ 20201201_W13_HW_sortW12randomPoker.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <stdio.h> /* printf, scanf, puts, NULL */
+#include <stdlib.h> /* srand, rand */
+#include <time.h> /* time */
 
 void outputPoker(int[]);
 
