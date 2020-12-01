44eebbb0b8912036f6df1618cb871acb8cf28d62
郵差 <a34007024@gmail.com>
2020-12-01 16:27:36 +0800

    generate original Poker and output

--- W12_HW_poker.c
+++ W12_HW_poker.c
@@ -1,2 +1,37 @@
 #include <stdio.h>
+#include <string.h>
 
+int main()
+{
+
+    int originalPoker[52];
+    for (int i = 0; i < 52; i++)
+    {
+        originalPoker[i] = i + 1;
+        //1 ~ 52
+    }
+
+    for (int i = 0; i < 52; i++)
+    {
+        if (originalPoker[i] <= 13)
+        {
+            printf("♠%d", originalPoker[i]);
+        }
+        else if (originalPoker[i] <= 26)
+        {
+            printf("♥%d ", originalPoker[i] % 13);
+        }
+        else if (originalPoker[i] <= 39)
+        {
+            printf("♦%d ", originalPoker[i] % 13);
+        }
+        else if (originalPoker[i] <= 52)
+        {
+            printf("♣%d ", originalPoker[i] % 13);
+        }
+    }
+
+    //printf("♠ ♥ ♦ ♣\n");
+
+    return 0;
+}
