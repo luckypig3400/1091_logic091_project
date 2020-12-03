8dba726cf07981d079da79b3066380e204c8cd36
郵差 <a34007024@gmail.com>
2020-12-03 13:46:15 +0800

    finish 2dices

--- W11_HW_2dices.c
+++ W11_HW_2dices.c
@@ -5,15 +5,23 @@
 int main()
 {
 
-    int randomNumber;
-
+    int randomNumber, totalDiceNumber = 0;
+    int dice[2];
     //initialize random seed
     srand(time(NULL)); //用時間將亂數表先打亂
 
     //generate random number between 1 to 6
     randomNumber = rand() % 6 + 1;
 
-    printf("%d", randomNumber);
+    printf("骰子點數為:");
+    for (int i = 0; i < 2; i++)
+    {
+        dice[i] = rand() % 6 + 1;
+        printf("%d ", dice[i]);
+        totalDiceNumber += dice[i];
+    }
+    printf("\n=========================\n");
+    printf("總和為%d", totalDiceNumber);
 
     return 0;
 }
