c4ac9940b0d139587cf567b9859811c00a0a914c
郵差 <a34007024@gmail.com>
2020-12-08 09:48:34 +0800

    add sortDice( )

--- W11_HW_3dices.c
+++ W11_HW_3dices.c
@@ -5,6 +5,7 @@
 int randomNumber, totalDiceNumber = 0;
 int dice[3];
 void tossDice();
+void sortDice();
 
 int main()
 {
@@ -15,8 +16,9 @@ int main()
         if (in_char == 'r' || in_char == 'R')
         {
             tossDice();
-            //TODO:判斷其點數。其基本規則
             //TODO:由小到大排列
+            sortDice();
+            //TODO:判斷其點數。其基本規則
         }
 
         printf("輸入R再投擲一次，或是輸入Q離開:");
@@ -42,6 +44,32 @@ void tossDice()
         dice[i] = rand() % 6 + 1;
         printf("%d ", dice[i]);
         totalDiceNumber += dice[i];
+    }
+    printf("\n=========================\n");
+}
+
+void sortDice()
+{
+    //printf("dice.len=%d", sizeof(dice) / sizeof(dice[0]));
+    //https://stackoverflow.com/questions/37538/how-do-i-determine-the-size-of-my-array-in-c
+    int arrayLen = sizeof(dice) / sizeof(dice[0]);
+    for (int i = 0; i < arrayLen; i++)
+    {
+        for (int j = 0; i < arrayLen-1; j++)
+        {
+            int swap = 0;
+            if (dice[j] > dice[j + 1])
+            {
+                swap = dice[j + 1];
+                dice[j + 1] = dice[j];
+                dice[j] = swap;
+            }
+        }
+    }
+    printf("排序後的骰子點數為:");
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%d ", dice[i]);
     }
     printf("\n=========================\n");
 }
