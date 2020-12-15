fe6baf6bd5f15851715d4deb09cc2de0e1ca3ce6
郵差 <a34007024@gmail.com>
2020-12-15 17:26:49 +0800

    add file in read write

--- 20201208_W14_HW_GuessNumberGame_with_fileRW.c
+++ 20201208_W14_HW_GuessNumberGame_with_fileRW.c
@@ -2,12 +2,28 @@
 #include <stdlib.h> // srand,rand
 #include <time.h>   //time
 
+FILE *file1;
+
 int main()
 {
 
     //TODO:開啟檔案，若無檔案則建立檔案，檔案預設值如下:
     //topPlayerName=預設第一名玩家
     //topPlayerGuessCount=66
+    char filename[60] = "20201208_W14_HW_GuessNumberGame_with_fileRW.txt";
+    char tempstr[32];
+
+    file1 = fopen(filename, "r");//讀取歷史檔案僅採用讀取模式(可以測試檔案是否存在)
+
+    if ((file1 == NULL))
+    {
+        printf("檔案發生錯誤(初次執行程式或是檔案毀損)\n");
+        file1 = fopen(filename, "w");
+        printf("建立預設檔案中...");
+        fputs("topPlayerName=預設第一名玩家\n", file1);
+        fputs("topPlayerGuessCount=66", file1);
+        exit(1); /* 強迫結束程式 */
+    }
 
     int secretNumber, userInput, guessCount = 0;
 
