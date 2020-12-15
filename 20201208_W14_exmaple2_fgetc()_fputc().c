dc084c2f29716113bb40dcf6d064e577f0c4f10e
郵差 <a34007024@gmail.com>
2020-12-15 16:45:05 +0800

    format file

--- 20201208_W14_exmaple2_fgetc()_fputc().c
+++ 20201208_W14_exmaple2_fgetc()_fputc().c
@@ -1,15 +1,20 @@
 /* 檔名:ch11_02.c 功能:複製檔案(使用fgetc與fputc) */
+
 #include <stdlib.h>
 #include <stdio.h>
+
 FILE *fp1, *fp2;
+
 void main(void)
 {
     char filename1[80], filename2[80];
     char c;
+
     printf("請輸入來源檔名:");
     scanf("%s", filename1);
     printf("請輸入目的檔名:");
     scanf("%s", filename2);
+
     fp1 = fopen(filename1, "r");
     fp2 = fopen(filename2, "w");
 
@@ -18,6 +23,7 @@ void main(void)
         printf("檔案發生錯誤\n");
         exit(1); /* 強迫結束程式 */
     }
+
     printf("讀取並寫入中......\n");
 
     // 以下迴圈為採用一個字元一個字元複製
@@ -27,8 +33,8 @@ void main(void)
         fputc(c, fp2); /* 使用迴圈寫入目的檔內容 */
     }
     // 以上迴圈為採用一個字元一個字元複製
-
     printf("\n讀取並寫入完畢......\n");
+
     fclose(fp1);
     fclose(fp2);
     /* system("pause"); */
