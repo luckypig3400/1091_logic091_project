30a35f7c5c02336ab187455fdc4640295caf8445
郵差 <a34007024@gmail.com>
2020-12-15 22:02:11 +0800

    read record file

--- 20201208_W14_HW_GuessNumberGame_with_fileRW.c
+++ 20201208_W14_HW_GuessNumberGame_with_fileRW.c
@@ -1,6 +1,7 @@
 #include <stdio.h>  // printf, scanf,puts,NULL
 #include <stdlib.h> // srand,rand
 #include <time.h>   //time
+#include <string.h>
 
 FILE *file1;
 
@@ -10,10 +11,14 @@ int main()
     //TODO:開啟檔案，若無檔案則建立檔案，檔案預設值如下:
     //topPlayerName=預設第一名玩家
     //topPlayerGuessCount=66
+    int secretNumber, userInput, guessCount = 0;
+    char topPlayerName[32];
+    char topPlayerGuessCount[3] = "66";
+    char fileContext[256] = "";
     char filename[60] = "20201208_W14_HW_GuessNumberGame_with_fileRW.txt";
     char tempstr[32];
 
-    file1 = fopen(filename, "r");//讀取歷史檔案僅採用讀取模式(可以測試檔案是否存在)
+    file1 = fopen(filename, "r"); //讀取歷史檔案僅採用讀取模式(可以測試檔案是否存在)
 
     if ((file1 == NULL))
     {
@@ -25,7 +30,27 @@ int main()
         exit(1); /* 強迫結束程式 */
     }
 
-    int secretNumber, userInput, guessCount = 0;
+    while (fgets(tempstr, 32, file1) != NULL) /* 使用迴圈讀取完整來源檔內容 */
+    {
+        
+    }
+
+    if (fileContext[13] == '=')
+    {
+        printf("正確的存檔存檔內容為:%s\n", fileContext);
+        int nameCharCount = 0;
+        for (int i = 14; i < 256; i++)
+        {
+            if (fileContext[i] == 't')
+            {
+                i += 18;
+            }
+            if (fileContext[i] == '=')
+                printf("找到最高紀錄的等於符號");
+            topPlayerName[nameCharCount] = fileContext[i];
+            nameCharCount += 1;
+        }
+    }
 
     srand(time(NULL)); //用時間將亂數表先打亂
 
@@ -51,6 +76,8 @@ int main()
 
     //TODO:如果此局猜數字次數比檔案中的還要好
     //(打破歷史紀錄)
+
+    system("pause");
 
     return 0;
 }
