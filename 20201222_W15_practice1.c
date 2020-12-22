42ab9b8358fc192628ad6275cb224b04269cb1cf
郵差 <a34007024@gmail.com>
2020-12-22 16:33:43 +0800

    create userAccount with number 00i

--- 20201222_W15_practice1.c
+++ 20201222_W15_practice1.c
@@ -5,10 +5,10 @@
 FILE *file1;
 char fileName[60] = "user.dat";
 
-void writeFile(char in_user[60], char in_pwd[60])
+void writeFile(char in_userAccount[60], char in_pwd[60])
 {
     char writeCache[256] = "";
-    strcat(writeCache, in_user);
+    strcat(writeCache, in_userAccount);
     strcat(writeCache, "\t");
     strcat(writeCache, in_pwd);
     strcat(writeCache, "\n");
@@ -19,5 +19,17 @@ int main()
 {
     file1 = fopen(fileName, "w");
 
+    for (int i = 1; i <= 100; i++)
+    {
+        char num[9];
+        sprintf(num, "%03d", i); //把i補0後存到num裡
+        // 小技巧:使用atio()可以把字串轉數字
+
+        char userAccount[60] = "HN";
+        strcat(userAccount, num);
+        printf("%s\n", userAccount);
+    }
+
+    fclose(file1);
     return 0;
 }
