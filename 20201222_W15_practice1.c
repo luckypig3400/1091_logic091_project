f3eeaf3b483e7e0333db167d21c19751e024fd7a
郵差 <a34007024@gmail.com>
2020-12-22 16:50:53 +0800

    add reference in comment

--- 20201222_W15_practice1.c
+++ 20201222_W15_practice1.c
@@ -24,6 +24,7 @@ int main()
         char num[9];
         sprintf(num, "%03d", i); //把i補0後存到num裡
         // 小技巧:使用atio()可以把字串轉數字
+        //https://bbs.csdn.net/topics/10129157
 
         char userAccount[60] = "HN";
         strcat(userAccount, num);
