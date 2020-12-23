19c7c74c97e5dbf54bd825b5b07746a3747119f2
郵差 <a34007024@gmail.com>
2020-12-23 18:26:36 +0800

    change open file code and test it

--- 20201222_W15_practice2.c
+++ 20201222_W15_practice2.c
@@ -6,12 +6,18 @@ FILE *file1;
 char fileName[60] = ".\\bin\\user.dat";
 
 int main(){
-    if(!(file1 = fopen(fileName, "r"))){
+
+    file1 = fopen(fileName, "r");
+
+    if(file1 == NULL){
         printf("無法開啟檔案 檔案損毀或不存在 將離開系統...");
         return -1;
     }
 
     else{
+        char tempStr[64];
+        char userAccAndPWD[100][64];
+
         printf("成功讀取檔案\n");
 
         return 0;
