ca490c6911636cf326deeb40786a1b087af4222a
郵差 <a34007024@gmail.com>
2020-12-23 18:35:17 +0800

    read all and store in String Array

--- 20201222_W15_practice2.c
+++ 20201222_W15_practice2.c
@@ -5,18 +5,28 @@
 FILE *file1;
 char fileName[60] = ".\\bin\\user.dat";
 
-int main(){
+int main()
+{
 
     file1 = fopen(fileName, "r");
 
-    if(file1 == NULL){
+    if (file1 == NULL)
+    {
         printf("無法開啟檔案 檔案損毀或不存在 將離開系統...");
         return -1;
     }
 
-    else{
+    else
+    {
         char tempStr[64];
         char userAccAndPWD[100][64];
+        int i = 0;
+        while (fgets(tempStr, 64, file1) != NULL)
+        {
+            strcat(userAccAndPWD[i], tempStr);
+            printf("%s", userAccAndPWD[i]);
+            i += 1;
+        }
 
         printf("成功讀取檔案\n");
 
