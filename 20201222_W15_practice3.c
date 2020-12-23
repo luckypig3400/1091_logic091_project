60d60f5d2dcd2fea48aea46411be741bb3647d9f
郵差 <a34007024@gmail.com>
2020-12-23 20:24:56 +0800

    add reference for practice3

---
+++ 20201222_W15_practice3.c
@@ -0,0 +1,7 @@
+// W15 HW : http://logic091.ntunhs.org/course/W15.pdf
+
+// string replace can refer to this page:
+// https://jax-work-archive.blogspot.com/2009/04/c-strreplace.html
+
+// string compare can refer to this page:
+// http://tw.gitbook.net/c_standard_library/c_function_strcmp.html
