47d7474c21f353752ac5597cc37bd43a469b94dd
郵差 <a34007024@gmail.com>
2020-12-23 19:35:08 +0800

    store Account and password in different strArray

--- 20201222_W15_practice2.c
+++ 20201222_W15_practice2.c
@@ -19,13 +19,22 @@ int main()
     else
     {
         char tempStr[64];
-        char userAccAndPWD[100][64];
-        memset(userAccAndPWD, '\0', 6400); //要先把變數的記憶體全部清空才不會出現亂碼
+        char userAccount[100][32];
+        char userPWD[100][32];
+        memset(userAccount, '\0', 3200); //要先把變數的記憶體全部清空才不會出現亂碼
+        memset(userPWD, '\0', 3200);
         int i = 0;
         while (fgets(tempStr, 64, file1) != NULL)
         {
-            strcat(userAccAndPWD[i], tempStr);
-            printf("%s", userAccAndPWD[i]);
+            for (int j = 0; j < 5; j++)
+            { // tempStr[5]為\t 不複製
+                userAccount[i][j] = tempStr[j];
+            }// 其實這樣寫不太好，因為這樣只能讀取符合標準格式與長度的帳號與密碼
+            for (int j = 0; j < 8; j++)
+            { // 密碼為8個字元從tempStr[6]開始
+                userPWD[i][j] = tempStr[6 + j];
+            }
+            printf("%s\t%s\n", userAccount[i], userPWD[i]);
             i += 1;
         }
 
