2042c282be7e2f33fcb169e5865fd3c68669b7a4
郵差 <a34007024@gmail.com>
2020-12-23 18:21:06 +0800

    open file in W15 practice2

--- 20201222_W15_practice2.c
+++ 20201222_W15_practice2.c
@@ -3,11 +3,17 @@
 #include <string.h>
 
 FILE *file1;
-char fileName[60] = "user.dat";
+char fileName[60] = ".\\bin\\user.dat";
 
 int main(){
+    if(!(file1 = fopen(fileName, "r"))){
+        printf("無法開啟檔案 檔案損毀或不存在 將離開系統...");
+        return -1;
+    }
 
+    else{
+        printf("成功讀取檔案\n");
 
-
-    return 0;
+        return 0;
+    }
 }
